a0c7a3b66af4f3547a5446c923861505d23e54b0
Noah Zinsmeister <noah@hydrogenplatform.com>
2018-06-21 17:33:52 -0400

    basic lightwallet code

--- README.md
+++ README.md
@@ -1,1 +1,31 @@
 # eth-wallet-light
+
+sample code:
+
+```javascript
+const ethWalletLight = require('ethWalletLight')
+
+var msgHash = "0x9c22ff5f21f0b81b113e63f7db6da94fedef11b2119b4088b89664fb9a3cb658"
+var password = "mypassword"
+var entropy = "2o3uhrb2i3pbrq32b"
+
+// initialize
+var keystore = new ethWalletLight.Keystore()
+keystore.initialize(entropy, password)
+
+// sign message
+var signature = ethWalletLight.concatSig(keystore.signMsgHash(msgHash, password))
+
+// serialize to string
+var serialized = ks1.serialize()
+
+// recover from serialized
+var keystore2 = new ethWalletLight.Keystore()
+ks2.fromSerialized(serialized)
+
+// sign message
+var signature2 = ethWalletLight.concatSig(keystore2.signMsgHash(msgHash, password))
+
+// get public address
+var address = ks1.address
+```
---
+++ index.js
@@ -0,0 +1,74 @@
+const bip39 = require('bip39')
+const CryptoJS = require('crypto-js')
+const ethUtil = require('ethereumjs-util')
+const ethWallet = require('ethereumjs-wallet')
+const hdkey = require('ethereumjs-wallet/hdkey')
+
+class Keystore {
+  initialize (entropy, password) {
+    var hashedEntropy = ethUtil.sha256(entropy).slice(0, 16);
+
+    var mnemonic = bip39.generateMnemonic(undefined, () => { return hashedEntropy })
+    if (!bip39.validateMnemonic(mnemonic)) {
+      throw new Error("invalid mnemonic")
+    }
+
+    var seed = bip39.mnemonicToSeed(mnemonic)
+    var hdWallet = hdkey.fromMasterSeed(seed).derivePath(`m/44'/60'/0'/0`)
+    var wallet = hdWallet.deriveChild(0).getWallet()
+
+    this.address = wallet.getAddressString()
+
+    this.encodedPrivateKey = this.encryptString(wallet.getPrivateKeyString(), password)
+    this.encodedMnemonic = this.encryptString(mnemonic, password)
+  }
+
+  encryptString (string, password) {
+    var ciphertext = CryptoJS.AES.encrypt(string, password)
+    return ciphertext.toString()
+  }
+
+  decryptString (ciphertext, password) {
+    var bytes = CryptoJS.AES.decrypt(ciphertext, password)
+    var plaintext = bytes.toString(CryptoJS.enc.Utf8)
+    return plaintext
+  }
+
+  serialize () {
+    return JSON.stringify({
+      publicKey: this.publicKey,
+      encodedPrivateKey: this.encodedPrivateKey,
+      encodedMnemonic: this.encodedMnemonic
+    })
+  }
+
+  fromSerialized (serializedKeystore) {
+    var variables = JSON.parse(serializedKeystore)
+    this.publicKey = variables.publicKey,
+    this.encodedPrivateKey = variables.encodedPrivateKey,
+    this.encodedMnemonic = variables.encodedMnemonic
+  }
+
+  signMsgHash (msgHash, password) {
+    var privKey = this.decryptString(this.encodedPrivateKey, password)
+    return ethUtil.ecsign(Buffer.from(ethUtil.stripHexPrefix(msgHash), 'hex'), Buffer.from(privKey.substring(2), 'hex'))
+  }
+}
+
+var concatSig = (signature) => {
+  var r = signature.r;
+  var s = signature.s;
+  var v = signature.v;
+  r = ethUtil.fromSigned(r);
+  s = ethUtil.fromSigned(s);
+  v = ethUtil.bufferToInt(v);
+  r = ethUtil.setLengthLeft(ethUtil.toUnsigned(r), 32).toString('hex');
+  s = ethUtil.setLengthLeft(ethUtil.toUnsigned(s), 32).toString('hex');
+  v = ethUtil.stripHexPrefix(ethUtil.intToHex(v));
+  return ethUtil.addHexPrefix(r.concat(s, v).toString("hex"));
+}
+
+module.exports = {
+  Keystore: Keystore,
+  concatSig: concatSig
+}
